7fb1c8a6bc07f55616c30d310bacaa3abe7fd416
AuntL1ke <88139445+AuntL1ke@users.noreply.github.com>
2024-04-16 07:16:01 -0700

    Add files via upload

---
+++ Home1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace client_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string address = "127.0.0.1";
+        private int port = 8080;
+        private IPEndPoint ipPoint;
+        private IPEndPoint remotePoint;
+        private UdpClient client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            remotePoint = new IPEndPoint(IPAddress.Any, 0);
+            client = new UdpClient();
+        }
+
+        private void Search_btn(object sender, RoutedEventArgs e)
+        {
+            string index = street_index.Text;
+            byte[] data = Encoding.Unicode.GetBytes(index);
+
+            client.Send(data, data.Length, ipPoint); // Відправити дані
+
+            string requestInfo = $"Sent request: {index}";
+            ListBoxItem item = new ListBoxItem();
+            item.Content = requestInfo;
+            listBox.Items.Add(item);
+
+            try
+            {
+                data = client.Receive(ref remotePoint); // Отримати відповідь
+                string response = Encoding.Unicode.GetString(data);
+                string responseInfo = $"Received response: {response}";
+                ListBoxItem responseItem = new ListBoxItem();
+                responseItem.Content = responseInfo;
+                listBox.Items.Add(responseItem);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show("Error receiving response: " + ex.Message);
+            }
+        }
+    }
+
+
+}
---
+++ Home1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+public class Street
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int StreetIndex { get; set; }
+}
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Street> Street { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = @"data source=DESKTOP-JGNME93;
+                                    initial catalog=DbStreet;
+                                    integrated security=True;
+                                    Connect Timeout=2;
+                                    Encrypt=False;
+                                    Trust Server Certificate=False;
+                                    Application Intent=ReadWrite;
+                                    Multi Subnet Failover=False";
+
+            optionsBuilder.UseSqlServer(connectionString, options =>
+            {
+                options.EnableRetryOnFailure();
+            });
+        }
+    }
+
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        string address = "127.0.00.1";
+        int port = 8080;
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        IPEndPoint remotePoint = new IPEndPoint(IPAddress.Any, 0);
+
+        UdpClient listener = new UdpClient(ipPoint);
+
+        try
+        {
+            Console.WriteLine("Server started! Waiting for connection .... ");
+
+            while (true)
+            {
+                int bytes = 0;
+                byte[] data = listener.Receive(ref remotePoint);
+                string msg = Encoding.Unicode.GetString(data);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()} :: {msg}");
+
+
+                int streetIndex = int.Parse(msg);
+                string message = "";
+                MyDbContext db = new MyDbContext();
+                var streets = db.Street
+                            .Where(e => e.StreetIndex == streetIndex)
+                            .Select(e => e.Name)
+                            .ToList();
+
+
+                foreach (var street in streets)
+                {
+                    Console.WriteLine($"Street: {street}");
+                    message += street + '\n';
+                }
+                
+
+        
+                data = Encoding.Unicode.GetBytes(message);
+                listener.Send(data, data.Length, remotePoint);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.ToString()}");
+        }
+    }
+}
